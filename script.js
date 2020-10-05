a464592e641a6f6f41c9b23dcb83868b45dd35dc
Zachary Lynn <zalynn25@hotmail.com>
2020-10-05 19:23:47 -0400

    update2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-alert("hi")
-var weathURL= "https://api.openweathermap.org/data/2.5/weather?lat=41.392780&lon=-82.014999&appid=cf895b6ea140ccd8bf7133bda48dc02f";
-var fiveDayURL="https://api.openweathermap.org/data/2.5/forecast?lat=41.392780&lon=-82.014999&appid=cf895b6ea140ccd8bf7133bda48dc02f";
+
+var weathURL= "https://api.openweathermap.org/data/2.5/weather?lat=41.392780&lon=-82.014999&units=imperial&appid=cf895b6ea140ccd8bf7133bda48dc02f";
+var fiveDayURL="https://api.openweathermap.org/data/2.5/forecast?lat=41.392780&lon=-82.014999&units=imperial&appid=cf895b6ea140ccd8bf7133bda48dc02f";
+var uvURL="http://api.openweathermap.org/data/2.5/uvi?lat=41.392780&lon=-82.01499&appid=cf895b6ea140ccd8bf7133bda48dc02f"
 $.ajax({
     url: weathURL,
     method: "GET",
@@ -8,7 +9,7 @@ $.ajax({
 .then(function(response) {
     console.log(response);
 });
-alert("good day")
+
 $.ajax({
     url: fiveDayURL,
     method: "GET",
@@ -17,3 +18,43 @@ $.ajax({
     console.log(response);
 });
 
+$.ajax({
+    url: uvURL,
+    method: "GET",
+})
+.then(function(response){
+    console.log(response);
+})
+
+$(".button").on("click", function() {
+   fetch(weathURL)
+   .then(response => response.json())
+   .then(data => {
+       
+
+       console.log(data.name);
+       console.log(data.main["temp"])
+
+    }) 
+
+   fetch(uvURL)
+   .then(response => response.json())
+   .then(data => {
+
+    console.log(data.value);
+   })  
+   
+   fetch(fiveDayURL)
+   .then(response => response.json())
+   .then(data => {
+
+    console.log(data.list[0].main["temp"]);
+    console.log(data.list[8].main["temp"]);
+    console.log(data.list[16].main["temp"]);
+    console.log(data.list[24].main["temp"]);
+    console.log(data.list[32].main["temp"]);
+    
+   })  
+   
+   
+})
