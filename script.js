8e6986993405c3fa1c4528c8cc24ec4008d17e49
Zachary Lynn <zalynn25@hotmail.com>
2020-10-04 21:52:36 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 alert("hi")
-var weathURL= "https://api.openweathermap.org/data/2.5/weather?lat=41.4&lon=-81&appid=cf895b6ea140ccd8bf7133bda48dc02f";
-
+var weathURL= "https://api.openweathermap.org/data/2.5/weather?lat=41.392780&lon=-82.014999&appid=cf895b6ea140ccd8bf7133bda48dc02f";
+var fiveDayURL="https://api.openweathermap.org/data/2.5/forecast?lat=41.392780&lon=-82.014999&appid=cf895b6ea140ccd8bf7133bda48dc02f";
 $.ajax({
     url: weathURL,
     method: "GET",
 })
 .then(function(response) {
     console.log(response);
+});
+alert("good day")
+$.ajax({
+    url: fiveDayURL,
+    method: "GET",
 })
+.then(function(response) {
+    console.log(response);
+});
+
